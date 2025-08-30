33ce2907d7c6935814bb2dbb4618fc6a86551f57
Aweiro <arsenskott@gmail.com>
2025-08-30 16:23:48 +0300

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface Props {
+  images: string[];
+  step: number;
+  itemWidth: number;
+  frameSize: number;
+  animationDuration: number;
+  infinite: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  itemWidth,
+  frameSize,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentStep, setCurrentStep] = useState(step);
+  const [currentWidth, setCurrentWidth] = useState(itemWidth);
+  const [currentSize, setCurrentSize] = useState(frameSize);
+  const [currentDuration, setCurrentDuration] = useState(animationDuration);
+  const [currentInfinite, setCurrentInfinite] = useState(infinite);
+  const size = currentSize * currentWidth;
+
+  return (
+    <div className="Carousel" style={{ width: `${size}px` }}>
+      <ul
+        className="Carousel__list"
+        style={{
+          transition: `transform ${currentDuration}ms ease`,
+          transform: `translateX(-${currentIndex * currentWidth}px)`,
+        }}
+      >
+        {images.map((a, i) => (
+          <li key={i}>
+            <img src={a} alt={a} style={{ width: `${currentWidth}px` }} />
+          </li>
+        ))}
+      </ul>
+
+      <button
+        type="button"
+        disabled={currentIndex <= 0 && currentInfinite !== true}
+        onClick={e => {
+          e.preventDefault();
+          if (currentInfinite) {
+            if (currentIndex <= 0) {
+              setCurrentIndex(images.length - currentSize);
+            } else {
+              setCurrentIndex(currentIndex - currentStep);
+            }
+          } else {
+            if (currentIndex - currentSize < 0) {
+              setCurrentIndex(0);
+            } else {
+              setCurrentIndex(currentIndex - currentStep);
+            }
+          }
+        }}
+      >
+        Prev
+      </button>
+      <button
+        type="button"
+        disabled={
+          currentIndex + currentSize >= images.length &&
+          currentInfinite !== true
+        }
+        onClick={e => {
+          e.preventDefault();
+          if (currentInfinite) {
+            if (currentIndex >= images.length - currentSize) {
+              setCurrentIndex(0);
+            } else {
+              setCurrentIndex(
+                Math.min(
+                  currentIndex + currentStep,
+                  images.length - currentSize,
+                ),
+              );
+            }
+          } else {
+            setCurrentIndex(
+              Math.min(currentIndex + currentStep, images.length - currentSize),
+            );
+          }
+        }}
+      >
+        Next
+      </button>
+
+      <label htmlFor="itemStep">itemStep</label>
+      <input
+        id="itemStep"
+        type="number"
+        min="1"
+        max={images.length}
+        defaultValue={step}
+        onChange={e => setCurrentStep(+e.target.value)}
+      />
+
+      <label htmlFor="itemWidth">itemWidth</label>
+      <input
+        id="itemWidth"
+        type="number"
+        min="1"
+        defaultValue={itemWidth}
+        onChange={e => setCurrentWidth(+e.target.value)}
+      />
+
+      <label htmlFor="frameSize">frameSize</label>
+      <input
+        id="frameSize"
+        type="number"
+        min="1"
+        max={images.length}
+        defaultValue={frameSize}
+        onChange={e => setCurrentSize(+e.target.value)}
+      />
+
+      <label htmlFor="animationDuration">Animation Duration</label>
+      <input
+        id="animationDuration"
+        type="number"
+        min="1"
+        defaultValue={animationDuration}
+        onChange={e => setCurrentDuration(+e.target.value)}
+      />
+
+      <label htmlFor="isInfinite">Infinite</label>
+      <input
+        id="isInfinite"
+        type="checkbox"
+        checked={currentInfinite}
+        onChange={e => setCurrentInfinite(e.target.checked)}
+      />
+    </div>
+  );
+};
 
 export default Carousel;
